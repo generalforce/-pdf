6a6a2ae93b408cbb9e1dc7e0ba0915654982074d
generalforce <eairdrop50@gmail.com>
2026-08-08 02:17:48 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: './',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+export default defineConfig(() => {
+  return {
+    base: './', // <--- این خط جادویی مشکل مسیرها را حل می‌کند
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
